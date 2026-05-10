22203ac59603a51d6bbc80cede6a1a1956d09bd4
Erdinc Pulat <erdincpulat@gmail.com>
2026-05-10 23:34:12 +0300

    Fix admin header and globals css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "../styles/globals.css"
+import "./globals.css";
 
 import SiteHeader from "@/components/layout/SiteHeader";
 import SiteFooter from "@/components/layout/SiteFooter";
